e60456a80db6ec4e28c17c5f5e056ed7e039b166
IKwesi <rbblankson34@gmail.com>
2020-06-06 05:12:18 +0700

    Updated interface for player

--- Helpers/IPlayer.py
+++ Helpers/IPlayer.py
@@ -2,8 +2,8 @@ from abc import ABC, abstractmethod
 
 class IPlayer(ABC):
     
-    # @abstractmethod
-    # def get_player_pos(self): pass
+    @abstractmethod
+    def get_player_pos(self): pass
 
     @abstractmethod
     def move(self): pass
